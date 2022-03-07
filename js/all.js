4cfb0b21955bc43d8aae5948e0d5364684cb201a
ml-michael <michael.zm@medialand.tw>
2022-03-07 18:44:30 +0800

    update data

--- js/all.js
+++ js/all.js
@@ -3,6 +3,8 @@ const titleSection = document.getElementById("title-section")
 const featureSmallItem = document.getElementById("featuresSmallItem")
 const featureBigItem = document.getElementById("featuresBigItem")
 const replaceName = document.getElementById("replaceName")
+const regularName = document.getElementById("regularName")
+const regularItem = document.getElementById("regularItem")
 const replaceItem = document.getElementById("replaceItem")
 const cardImgSection = document.getElementById("card-img-section")
 
@@ -10,33 +12,40 @@ const cardImgSection = document.getElementById("card-img-section")
 cardTitle.textContent = Page_Data.pageName;
 titleSection.textContent = Page_Data.featureName;
 replaceName.textContent = Page_Data.replaceName
+regularName.textContent = Page_Data.replaceRegular
 cardImgSection.innerHTML = `
             <img 
             class="card-img-top img-fluid" id="card-image" alt="picture" 
-            src=\"./image/${Page_Data.imageName}.jpeg"\>
+            src=\"./image/${Page_Data.imageName}.png"\>
         `
 // features item
 for (let index = 0; index < Page_Data.featuresArray.length; index++) {
     // console.log(Page_Data.featuresArray)
-    featureSmallItem.innerHTML += `
-            <li class="text-start" id="featuresSmallItem">    
-                ${Page_Data.featuresArray[index]}
-            </li>`
+    featureSmallItem.innerHTML += `${Page_Data.featuresArray[index]}`
 }
 // features big item
 
-for (let index = 0; index < Page_Data.featuresBigArray.length; index++) {
-    // console.log(Page_Data.featuresArray)
-    featuresBigItem.innerHTML += `
-            <span class="text-start big-item " id="featuresBigItem">    
-                ${Page_Data.featuresBigArray[index]}
-            </span>`
-}
+// for (let index = 0; index < Page_Data.featuresBigArray.length; index++) {
+//     // console.log(Page_Data.featuresArray)
+//     featuresBigItem.innerHTML += `
+//             ${Page_Data.featuresBigArray[index]}`
+// }
 // replace item
 for (let index = 0; index < Page_Data.replaceArray.length; index++) {
     // console.log(Page_Data.replaceArray)
     replaceItem.innerHTML += `
-            <li class="text-start" id="replaceItem">    
-                ${Page_Data.replaceArray[index]}
-            </li>`
+    
+    <li class="text-start" id="replaceItem">
+    ${Page_Data.replaceArray[index]}
+    </li>
+    `
+}
+for (let index = 0; index < Page_Data.regularArray.length; index++) {
+    // console.log(Page_Data.replaceArray)
+    regularItem.innerHTML += `
+    <li class="text-start" id="replaceItem">
+    ${Page_Data.regularArray[index]}
+    </li>
+    
+    `
 }
